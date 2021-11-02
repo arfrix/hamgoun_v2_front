7c954e293ccf99f8cf25e0aedf0bc23b2e5396bc
arfa <arfa.madi@tpasi.cab>
2021-11-02 11:42:00 +0330

    header and footer is imported and used

--- src/layouts/HeaderBodyFooterLayout.jsx
+++ src/layouts/HeaderBodyFooterLayout.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import Header from '../blocks/header/HeaderView.jsx'
+import Footer from '../blocks/footer/FooterView.jsx'
 
 export default function HeaderBodyFooterLayout({children}) {
     return (
         <section className="HBF-main">
             <article className="HBF-main__header">
-                {/* <Header/> */}
+                <Header/>
             </article>
             <article className="HBF-main__body">
                 {children}
             </article>
             <article className="HBF-main__footer">
-                {/* <Footer/> */}
+                <Footer/>
             </article>
         </section>
     )
