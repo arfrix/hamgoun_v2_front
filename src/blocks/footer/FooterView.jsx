0ec9553dc3be64eb481a0c62e8ec0e0184b11cfd
arfa <arfa.madi@tpasi.cab>
2021-11-02 11:56:14 +0330

    footer and header componets are created and used

---
+++ src/blocks/footer/FooterView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FooterView() {
+    return (
+        <div>
+            footer
+        </div>
+    )
+}
--- src/layouts/HeaderBodyFooterLayout.jsx
+++ src/layouts/HeaderBodyFooterLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from '../blocks/header/HeaderView.jsx'
-import Footer from '../blocks/footer/FooterView.jsx'
+import Header from '../blocks/header/HeaderView'
+import Footer from '../blocks/footer/FooterView'
 
 export default function HeaderBodyFooterLayout({children}) {
     return (
